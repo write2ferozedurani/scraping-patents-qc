630f23f45b256083528d306591f1de5c26a48017
write2ferozedurani <write2ferozedurani@gmail.com>
2022-02-22 02:43:41 -0800

    updated

---
+++ scraping-patents/src/app/common/GlobalConstants.ts
@@ -0,0 +1,7 @@
+export class GlobalConstants {
+    //public static REST_PATENT_LIST_URL: string = 'api/patents/list';
+    //public static REST_PATENT_PROCESS_URL: string = 'api/patents/process';
+
+    public static REST_PATENT_LIST_URL: string = 'patents/list';
+    public static REST_PATENT_PROCESS_URL: string = 'patents/process';
+}
